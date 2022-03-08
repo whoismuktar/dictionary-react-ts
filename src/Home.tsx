4ce63d0cc02602b7f03cda3fafd3b90b4f7bf8bc
Muktar <mayunited12@yahoo.com>
2022-03-08 21:04:32 +0100

    Home progress

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import logo from './images/logo.svg';
+// import logo from './images/logo.svg';
+import Search from "./components/Search"
 
 export default function Home() {
   return (
-    <div>
-        <img src={logo} className="App-logo" alt="logo" />
-
-          <a
+    <div className='home'>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <a
               className="App-link"
               href="https://reactjs.org"
               target="_blank"
               rel="noopener noreferrer"
           >
               Learn React
-          </a>
+          </a> */}
+
+        <Search />
     </div>
   )
 }
