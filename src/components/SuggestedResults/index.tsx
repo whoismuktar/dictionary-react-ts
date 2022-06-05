db853b8309eee80dac55e1c218e067a400bc61f4
Muktar <mayunited12@yahoo.com>
2022-06-05 23:25:18 +0100

    implement result, noresult render

--- src/components/SuggestedResults/index.tsx
+++ src/components/SuggestedResults/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from "./suggested.module.scss"
 
 interface Props {
@@ -6,20 +6,42 @@ interface Props {
   data: unknown;
   dataErr: unknown;
 }
+interface Result {
+  word: string;
+}
 
 export default function SuggestedResults(props:Props) {
   const {loading, data, dataErr} = props
+  const [resultFound, setResultFound] = useState<number>(0)
+  const [noResult, setNoResult] = useState<number>(0)
+
+  useEffect(()=> {
+    setResultFound((data as []).length)
+    setNoResult(Object.keys(data as object).length)
+  }, [data])
+
   // console.log("active", loading)
   // console.log("data", data)
   console.log("dataErr", dataErr)
 
+  const Result = <ul className={styles["suggested-lists results"]}>
+  {
+    (data as []).map((result:Result, i:number) => {
+      return(
+        <li key={i} className={styles["__list"]}>{result.word}</li>
+      )
+    })
+  }
+</ul>
+
   return (
     <div className={`mx-auto my-0 transition duration-500 ease-linear ${styles["suggested-results"]}`}>
       <div className={styles["__title"]}>
-        { data != null ? "Results" : "Recent Searches"}
+        { resultFound || noResult ? "Results" : "Recent Searches"}
       </div>
 
       {
+        resultFound ? Result :
         loading ? 
         <div className={`animate-pulse py-5 ${styles["suggested-lists"]}`}>
           <div className="flex-1 space-y-5 py-1">
@@ -33,13 +55,20 @@ export default function SuggestedResults(props:Props) {
               )
             }
           </div>
-        </div> :
-        // <div>1</div>
-        <ul className={`${styles["suggested-lists"]}`}>
-          <li className={styles["__list"]}>Monogamy</li>
-          <li className={styles["__list"]}>Computer Science</li>
-          <li className={styles["__list"]}>Knowledge</li>
-        </ul>
+        </div> 
+        :
+        <div>No Result found {noResult}</div>
+        // <div>
+        //   { data === [] && "Recents Searches...."}
+        // </div> :
+
+        // Object.keys(data).length > 0 ? 
+        // <div>1</div> :
+        // <ul className={`${styles["suggested-lists"]}`}>
+        //   <li className={styles["__list"]}>Monogamy</li>
+        //   <li className={styles["__list"]}>Computer Science</li>
+        //   <li className={styles["__list"]}>Knowledge</li>
+        // </ul>
       }
 
       {/* {
